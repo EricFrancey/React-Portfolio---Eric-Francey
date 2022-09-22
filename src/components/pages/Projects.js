c76e85f1f206051a91d83cf64d4b366ae84b89bf
Eric Francey <eric.g.francey@gmail.com>
2022-09-22 01:31:25 -0400

    projects update

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,9 +4,17 @@ export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>
-        Coming soon
-      </p>
+      <ui>
+        <li>
+          <a href="https://ericfrancey.github.io/Rhythm-Finder/">Rhythm Rocks</a>
+        </li>
+        <li>
+          <a href="https://ericfrancey.github.io/drag-drop-photo-editor/">Photo Editor</a>
+        </li>
+        <li>
+          <a href="https://ericfrancey.github.io/ZooLogic/">ZooLogic</a>
+        </li>
+        </ui>
     </div>
   );
 }
